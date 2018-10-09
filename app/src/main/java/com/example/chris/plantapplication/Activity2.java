375590ffda6680692ad432b6eb1747cba962ff28
Chris Moen <moenchris1@gmail.com>
2018-10-09 07:04:53 -0400

    Adding the UI Layout elements

--- app/src/main/java/com/example/chris/plantapplication/Activity2.java
+++ app/src/main/java/com/example/chris/plantapplication/Activity2.java
@@ -1,13 +1,41 @@
 package com.example.chris.plantapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.app.Activity;
 
 public class Activity2 extends AppCompatActivity {
-
+    private ImageButton button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+        button = (ImageButton) findViewById(R.id.imageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //If this button is clicked, we also need to change the previous button pressed from the other screen
+                //If this button is clicked, then we will open activity2
+
+                openMainActivity();
+            }
+        });
+
+    }
+
+    public void openMainActivity() { //We want to open that activity and navigate over to the specific class
+        int buttonID= getIntent().getIntExtra("Button ID",0); //get the button session ID so we can modify its .xml paramaters
+
+        ImageButton btn_tmp;
+        btn_tmp = (ImageButton)findViewById(buttonID);
+      //  btn_tmep
+       // btn_tmp.setImageResource(R.drawable.eyeme); //set that button's ID to be the eye icon
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/chris/plantapplication/MainActivity.java
+++ app/src/main/java/com/example/chris/plantapplication/MainActivity.java
@@ -1,13 +1,56 @@
 package com.example.chris.plantapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.Activity;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageButton button;
+    private ImageButton button1;
+    private ImageButton button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (ImageButton) findViewById(R.id.imageButton13);
+        button1 = (ImageButton) findViewById(R.id.imageButton10);
+        button2 = (ImageButton) findViewById(R.id.imageButton9);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //If this button is clicked, then we will open activity2
+                openActivity2(button.getId());
+            }
+
+        });
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //If this button is clicked, then we will open activity2
+                openActivity2(button1.getId());
+            }
+
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //If this button is clicked, then we will open activity2
+                openActivity2(button2.getId());
+            }
+
+        });
+    }
+    public void openActivity2(int buttonID) {
+
+        Intent intent = new Intent(this, Activity2.class);
+        intent.putExtra("Button ID", buttonID);
+
+        startActivity(intent);
     }
 }
